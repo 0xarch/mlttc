0ec86d355d4fd2db377657d3e7cad9e8b1c60315
ml-308 <3082033429@qq.com>
2026-06-08 12:23:02 +0800

    Add files via upload

--- register.js
+++ register.js
@@ -11,6 +11,8 @@ function emailValidation(){
     if (t > 0 && e > 0 && t < e) {
         passget(username);
         document.getElementById('passwordMsg').innerHTML=pass+"1145";
+        document.getElementById("username").style.borderColor = "#1eff00";
+        document.getElementById("usernameMsg").style.display = "block";
         if(pass!=null){
         document.getElementById("username").style.borderColor = "#1eff00";
         document.getElementById("usernameMsg").style.display = "block";
